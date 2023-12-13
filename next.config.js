2d507fc0f51a30ed6212f8b0fb8ba957e48c31f2
Fhmi00 <anisfahmi045@gmail.com>
2023-12-13 22:07:45 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        domains: ["res.cloudinary.com"],
-    },
-    env: {
-        URL_BACKEND: "https://fazzpay-rose.vercel.app",
-        URL_CLOUDINARY: "https://res.cloudinary.com/dd1uwz8eu/image/upload/v1666604839",
-    },
+  reactStrictMode: true,
+  swcMinify: false,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  env: {
+    URL_BACKEND: "https://fazzpay-rose.vercel.app",
+    URL_CLOUDINARY:
+      "https://res.cloudinary.com/dd1uwz8eu/image/upload/v1666604839",
+  },
 };
 
 module.exports = nextConfig;
